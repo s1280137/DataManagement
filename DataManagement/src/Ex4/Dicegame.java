2abc0923da06e953e5b77734d14e323c0aa2b799
s1280137 <s1280137@u-aizu.ac.jp>
2021-07-07 15:45:17 +0900

    Initial revision

--- DataManagement/src/Ex4/Dicegame.java
+++ DataManagement/src/Ex4/Dicegame.java
@@ -1,10 +1,20 @@
 package Ex4;
 
+import java.util.Random;
+
 public class Dicegame {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+     Random random = new Random();
+     int randomValue1 = random.nextInt(6);
+     int randomValue2 = random.nextInt(6);
+     int sum = randomValue1 + randomValue2;
 
+     System.out.println("サイコロを振ると...");
+     System.out.println("サイコロ1：" + randomValue1);
+     System.out.println("サイコロ2：" + randomValue2);
+     System.out.println("合計値:" + sum);
 	}
 
 }
